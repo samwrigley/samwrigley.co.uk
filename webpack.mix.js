20b6401ff006354765ce50ca852207f13fc44174
Sam Wrigley <sam@samwrigley.co.uk>
2018-02-19 22:38:44 +0000

    Ensured all images within the `/assets` directory are copied over to the `public` directory

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,6 +14,7 @@ const mix = require('laravel-mix')
 
 mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
+    .copyDirectory('resources/assets/images', 'public/images')
     .version()
     .sourceMaps()
 
