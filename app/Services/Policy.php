1a6f90c1d9c79cf8a174d0d0d0fdd72517a803d4
Sam Wrigley <sam@samwrigley.co.uk>
2018-05-29 22:01:29 +0100

    Created custom content security policy

---
+++ app/Services/Policy.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Services\Csp;
+
+use Spatie\Csp\Directive;
+use Spatie\Csp\Policies\Policy as BasePolicy;
+
+class Policy extends BasePolicy
+{
+    public function configure()
+    {
+        $this->addGeneralDirectives();
+        $this->addDirectivesForGoogleFonts();
+        $this->addDirectivesForGoogleAnalytics();
+        $this->addDirectivesForGoogleTagManager();
+    }
+
+    protected function addGeneralDirectives(): self
+    {
+        return $this
+            ->addDirective(Directive::BASE, 'self')
+            ->addNonceForDirective(Directive::SCRIPT);
+    }
+
+    protected function addDirectivesForGoogleFonts(): self
+    {
+        return $this
+            ->addDirective(Directive::FONT, 'fonts.gstatic.com')
+            ->addDirective(Directive::SCRIPT, 'fonts.googleapis.com')
+            ->addDirective(Directive::STYLE, 'fonts.googleapis.com');
+    }
+
+    protected function addDirectivesForGoogleAnalytics(): self
+    {
+        return $this->addDirective(Directive::SCRIPT, '*.google-analytics.com');
+    }
+
+    protected function addDirectivesForGoogleTagManager(): self
+    {
+        return $this->addDirective(Directive::SCRIPT, '*.googletagmanager.com');
+    }
+}
