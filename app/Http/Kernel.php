7410f742e819c1dd2ed50c3a1d741761fbaa8cfd
Sam Wrigley <sam@samwrigley.co.uk>
2018-06-01 23:08:57 +0100

    Created custom `AddSecurityHeaders` middleware to add security headers to all web requests

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,6 +36,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \Spatie\Csp\AddCspHeaders::class,
+            \App\Http\Middleware\AddSecurityHeaders::class,
         ],
 
         'api' => [
