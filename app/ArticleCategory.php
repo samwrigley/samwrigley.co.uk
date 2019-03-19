6b0561817f6046022138271cf7943e62cd13c584
Sam Wrigley <sam@samwrigley.co.uk>
2019-03-19 21:25:43 +0000

    Create `ArticleCategory` model

---
+++ app/ArticleCategory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App;
+
+use App\Article;
+use App\Model;
+use App\Traits\HasArticles;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class ArticleCategory extends Model
+{
+    use HasArticles;
+
+    /**
+     * The route namespace.
+     *
+     * @var string[string]
+     */
+    protected $namespaces = [
+        'web' => 'article.categories.',
+        'admin' => 'admin.article.categories.',
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'slug',
+        'name',
+        'description',
+    ];
+
+    public function articles(): BelongsToMany
+    {
+        return $this->belongsToMany(Article::class);
+    }
+}
