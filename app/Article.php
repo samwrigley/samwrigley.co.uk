b943e98fb5b11bab8aad1572f10e6a7ecbe095eb
Sam Wrigley <sam@samwrigley.co.uk>
2019-03-19 21:22:31 +0000

    Create `Article` model

---
+++ app/Article.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App;
+
+use App\ArticleCategory;
+use App\Model;
+use App\User;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use SamWrigley\Support\Traits\CanBePublished;
+use SamWrigley\Support\Traits\HasAuthor;
+use SamWrigley\Support\Traits\HasCategories;
+
+class Article extends Model
+{
+    use HasAuthor;
+    use HasCategories;
+    use CanBePublished;
+
+    /**
+     * The route namespace.
+     *
+     * @var string[string]
+     */
+    protected $namespaces = [
+        'web' => 'articles.',
+        'admin' => 'admin.articles.',
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'title',
+        'slug',
+        'body',
+        'excerpt',
+    ];
+
+    /**
+     * The relations to eager load on every query.
+     *
+     * @var string[]
+     */
+    protected $with = [
+        'categories',
+    ];
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(ArticleCategory::class);
+    }
+}
