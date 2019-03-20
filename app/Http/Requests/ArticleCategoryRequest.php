cd21a6b96c68ed94fe206ba44017c34d81b1062b
Sam Wrigley <sam@samwrigley.co.uk>
2019-03-20 21:19:53 +0000

    Created `ArticleCategoryRequest` form request class

---
+++ app/Http/Requests/ArticleCategoryRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class ArticleCategoryRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        $category = $this->route('category');
+        $categoryId = optional($category)->id;
+
+        return [
+            'name' => [
+                'required',
+                'string',
+                'max:50',
+                Rule::unique('categories')->ignore($categoryId),
+            ],
+            'slug' => [
+                'required',
+                'string',
+                'max:50',
+                Rule::unique('categories')->ignore($categoryId),
+            ],
+            'description' => 'nullable|max:500',
+        ];
+    }
+}
