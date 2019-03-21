91b99dd92264eb90dab36d4b0bd8dc90fc6fe854
Sam Wrigley <sam@samwrigley.co.uk>
2019-03-21 22:00:50 +0000

    Corrected how `ArticleFactory` class assigns a user to given article

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -5,13 +5,12 @@ use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(Article::class, function (Faker $faker) {
-    $user = User::all()->random();
     $title = $faker->sentence;
     $created_at = $faker->dateTimeThisYear();
     $published_at = $faker->boolean(80) ? $faker->dateTimeBetween($created_at, 'now') : null;
 
     return [
-        'user_id' => $user->id,
+        'user_id' => factory(User::class),
         'title' => $title,
         'slug' => str_slug($title),
         'body' => $faker->paragraphs(10, true),
