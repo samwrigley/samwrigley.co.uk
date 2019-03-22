20f61fd433dffb92011c733ca02b72ca6b169f64
Sam Wrigley <sam@samwrigley.co.uk>
2019-03-22 08:05:18 +0000

    Updated `User` model

--- app/User.php
+++ app/User.php
@@ -2,28 +2,59 @@
 
 namespace App;
 
+use App\Article;
+use App\Traits\HasManyArticles;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use SamWrigley\Support\Traits\HasPaths;
 
 class User extends Authenticatable
 {
+    use HasPaths;
+    use HasManyArticles;
     use Notifiable;
+
+    /**
+     * The route namespace.
+     *
+     * @var string[]
+     */
+    protected $namespaces = [
+        'web' => 'users.',
+        'admin' => 'admin.users.',
+    ];
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var array
+     * @var string[]
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'slug',
+        'name',
+        'email',
+        'password',
+        'bio',
     ];
 
     /**
      * The attributes that should be hidden for arrays.
      *
-     * @var array
+     * @var string[]
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
+
+    public function articles(): HasMany
+    {
+        return $this->hasMany(Article::class);
+    }
+
+    public function publish(Article $article): void
+    {
+        $this->articles()->save($article);
+    }
 }
