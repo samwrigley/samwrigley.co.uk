68c5a478c9122ec9ea2512123377777b6553ddfe
Sam Wrigley <sam@samwrigley.co.uk>
2019-04-02 21:07:01 +0100

    Created `ViewServiceProvider` into to create `date` Blade directive to ensure consistent date formatting inside Blade templates

---
+++ app/Providers/ViewServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider;
+
+class ViewServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        /*
+         * Format given string instance into a date string.
+         *
+         * @example @date($value)
+         */
+        Blade::directive('date', function (string $date): string {
+            return "<?php echo ($date)->toFormattedDateString(); ?>";
+        });
+    }
+}
