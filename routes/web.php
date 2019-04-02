eb0760db3e95c1fb8787f58424f144fdca32ebe4
Sam Wrigley <sam@samwrigley.co.uk>
2019-04-02 21:16:24 +0100

    Swapped around article and article category routes to ensure article category route are accessible

--- routes/web.php
+++ routes/web.php
@@ -19,15 +19,15 @@ Route::get('/', function () {
 
 // Blog
 Route::group(['prefix' => 'blog', 'as' => 'blog.'], function () {
-    // Articles
-    Route::group(['prefix' => 'articles', 'as' => 'articles.'], function () {
-        Route::get('/', [ArticleController::class, 'index'])->name('index');
-        Route::get('{article}', [ArticleController::class, 'show'])->name('show');
-    });
-
     // Categories
     Route::group(['prefix' => 'categories', 'as' => 'categories.'], function () {
         Route::get('/', [ArticleCategoryController::class, 'index'])->name('index');
         Route::get('{category}', [ArticleCategoryController::class, 'show'])->name('show');
+    });
+
+    // Articles
+    Route::group(['as' => 'articles.'], function () {
+        Route::get('/', [ArticleController::class, 'index'])->name('index');
+        Route::get('{article}', [ArticleController::class, 'show'])->name('show');
     });
 });
