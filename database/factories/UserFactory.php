4d4e9a07614b535611634567b86892920d4f9bc5
Sam Wrigley <sam@samwrigley.co.uk>
2019-04-02 21:13:51 +0100

    Reduced length of user bio in `UserFactory` as was unrealistically long

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -11,7 +11,7 @@ $factory->define(User::class, function (Faker $faker) {
         'slug' => str_slug($name),
         'email' => $faker->unique()->safeEmail,
         'password' => bcrypt('secret'),
-        'bio' => $faker->paragraphs(3, true),
+        'bio' => $faker->paragraph,
         'avatar' => $faker->imageUrl(200, 200),
         'remember_token' => str_random(10),
     ];
