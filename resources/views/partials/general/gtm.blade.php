bba478895536156638cc1cce19794d7af99abb9f
Sam Wrigley <sam@samwrigley.co.uk>
2019-04-02 21:18:06 +0100

    Removed calls to Google Tag Manager environment variables and instead replaced with Google Tag Manager service configuration to ensure environment variables can be cached in production

--- resources/views/partials/general/gtm.blade.php
+++ resources/views/partials/general/gtm.blade.php
@@ -1,3 +1,3 @@
-@if (env('GOOGLE_TAG_MANAGER_ID'))
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{ env('GOOGLE_TAG_MANAGER_ID') }}" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+@if (config('services.google_tag_manager.id'))
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{ config('services.google_tag_manager.id') }}" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 @endif
