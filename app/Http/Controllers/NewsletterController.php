faa1f8c0ab0b121b5d825bbaf04d074e9323a718
Sam Wrigley <sam@samwrigley.co.uk>
2019-04-04 19:23:45 +0100

    Extract newsletter request validation into dedicated form request class

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -2,19 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\NewsletterRequest;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
 class NewsletterController extends Controller
 {
-    public function store(Request $request): RedirectResponse
+    public function store(NewsletterRequest $request): RedirectResponse
     {
-        $validated = $request->validate([
-            'email' => 'required|email',
-        ]);
+        Log::info("Newsletter subscription: {$request->email}");
 
-        Log::info("Newsletter subscription: {$validated['email']}");
 
         return redirect()->back();
     }
