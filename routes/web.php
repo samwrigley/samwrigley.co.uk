bcc3a6e488f722df30f277c8064d16cacbd94049
Sam Wrigley <sam@samwrigley.co.uk>
2019-04-04 20:27:32 +0100

    Removed unnecessary comment block

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,6 @@ use App\Http\Controllers\ArticleCategoryController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\NewsletterController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('pages.landing');
 });
