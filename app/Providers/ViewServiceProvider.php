8529d982ffb96123950ab6cc73b5260381fb9a50
Sam Wrigley <sam@samwrigley.co.uk>
2019-07-16 18:11:17 +0100

    Changed format of `date` Blade directive

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -15,7 +15,7 @@ class ViewServiceProvider extends ServiceProvider
          * @example @date($value)
          */
         Blade::directive('date', function (string $date): string {
-            return "<?php echo ($date)->toFormattedDateString(); ?>";
+            return "<?php echo ($date)->format('jS F Y'); ?>";
         });
     }
 }
