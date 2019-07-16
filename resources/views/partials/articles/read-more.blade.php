251d8380426df658f719c268810db17c3010df51
Sam Wrigley <sam@samwrigley.co.uk>
2019-07-16 18:36:22 +0100

    Removed Blade partials as no longer needed

--- resources/views/partials/articles/read-more.blade.php
+++
@@ -1,8 +0,0 @@
-<a href="{{ $article->showPath() }}"
-    class="article-overview__cta"
-    aria-label="@lang("Read: $article->title")"
-    title="@lang("Read: $article->title")"
->
-    @lang('Read more')
-    @svg('arrow-right')
-</a>
--- resources/views/partials/articles/title.blade.php
+++
@@ -1,8 +0,0 @@
-<h1 class="article__title">
-    <a href="{{ $article->showPath() }}"
-        aria-label="@lang("Read: $article->title")"
-        title="@lang("Read: $article->title")"
-    >
-        {{ $article->title }}
-    </a>
-</h1>
