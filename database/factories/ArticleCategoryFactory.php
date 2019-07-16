3c5c30b2cdac4e0c49ed99b7cbf1e1195a5a008e
Sam Wrigley <sam@samwrigley.co.uk>
2019-07-16 18:13:07 +0100

    Increase length of article category name generated by the article category factory to ensure category names are unique

--- database/factories/ArticleCategoryFactory.php
+++ database/factories/ArticleCategoryFactory.php
@@ -4,7 +4,7 @@ use App\ArticleCategory;
 use Faker\Generator as Faker;
 
 $factory->define(ArticleCategory::class, function (Faker $faker) {
-    $name = $faker->unique()->word;
+    $name = $faker->unique()->words(2, true);
 
     return [
         'slug' => str_slug($name),
