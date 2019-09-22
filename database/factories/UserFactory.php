e8b78fd015440d473d0dcc2789d44d48e40a93db
Sam Wrigley <sam@samwrigley.co.uk>
2019-09-22 16:46:27 +0100

    Refactored string and array global helpers to corresponding classes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,17 +2,18 @@
 
 use App\User;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     $name = $faker->unique()->name;
 
     return [
         'name' => $name,
-        'slug' => str_slug($name),
+        'slug' => str::slug($name),
         'email' => $faker->unique()->safeEmail,
         'password' => bcrypt('secret'),
         'bio' => $faker->paragraph,
         'avatar' => $faker->imageUrl(200, 200),
-        'remember_token' => str_random(10),
+        'remember_token' => Str::random(10),
     ];
 });
