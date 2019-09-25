eaa9a0a4313df34546b4ed336bff14b84382f358
Sam Wrigley <sam@samwrigley.co.uk>
2019-09-25 22:11:17 +0100

    Removed unused `use` statements

--- app/Traits/BelongsToManyArticles.php
+++ app/Traits/BelongsToManyArticles.php
@@ -3,8 +3,6 @@
 namespace App\Traits;
 
 use App\Traits\HasArticles;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 trait BelongsToManyArticles
 {
