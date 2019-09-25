31d6eedabd1157851fb61e9ce0827242a9060ca0
Sam Wrigley <sam@samwrigley.co.uk>
2019-09-25 22:29:44 +0100

    Ensured `featureArticle` variable is set before outputting featured article markup

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,72 +1,74 @@
 @extends('layouts.default')
 
 @section('content')
-    <div class="flex mb-12">
-        <div class="w-2/3">
-            <a href="{{ $featuredArticle->showPath() }}"
-                class="group block relative bg-gray-900 hover:bg-gray-800"
-            >
-                <article>
-                    <div class="w-1/2 p-12 pb-64">
-                        @if ($featuredArticle->categories->count())
-                            <ul class="mb-6 text-sm text-gray-500 uppercase tracking-widest">
-                                @foreach ($featuredArticle->categories as $category)
-                                    <li class="leading-none">
-                                        {{ $category->name }}
-                                    </li>
-                                @endforeach
-                            </ul>
-                        @endif
-
-                        <h2 class="mb-4 text-3xl text-white font-black">
-                            {{ $featuredArticle->title }}
-                        </h2>
-
-                        @if ($featuredArticle->published_at)
-                            <time datetime="{{ $featuredArticle->published_at }}"
-                                aria-label="@lang('Posted On')"
-                                class="block mb-6 text-base text-gray-500"
-                            >
-                                @date($featuredArticle->published_at)
-                            </time>
-                        @endif
-
-                        @if ($featuredArticle->excerpt)
-                            <p class="text-xl text-gray-200 font-light">
-                                {!! $featuredArticle->excerpt !!}
-                            </p>
-                        @endif
-                    </div>
-
-                    @if ($featuredArticle->isNew())
-                        <div class="absolute top-0 right-0 flex items-center justify-center bg-yellow-400 w-16 h-16 font-medium"
-                            title="@lang('This article was published recently')"
-                        >
-                            New
-                        </div>
-                    @endif
-
-                    <div class="absolute right-0 bottom-0 flex items-center justify-center bg-gray-200 text-xl w-16 h-16
-                        group-hover:bg-yellow-400"
+    @if ($featuredArticle)
+        <div class="flex mb-12">
+                <div class="w-2/3">
+                    <a href="{{ $featuredArticle->showPath() }}"
+                        class="group block relative bg-gray-900 hover:bg-gray-800"
                     >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 32 32"
-                            class="w-6"
-                        >
-                            <path d="M18 6l-1.43 1.39L24.15 15H3v2h21.15l-7.58 7.57L18 26l10-10L18 6z" />
-                        </svg>
-                    </div>
-                </article>
-            </a>
-        </div>
+                        <article>
+                            <div class="w-1/2 p-12 pb-64">
+                                @if ($featuredArticle->categories->count())
+                                    <ul class="mb-6 text-sm text-gray-500 uppercase tracking-widest">
+                                        @foreach ($featuredArticle->categories as $category)
+                                            <li class="leading-none">
+                                                {{ $category->name }}
+                                            </li>
+                                        @endforeach
+                                    </ul>
+                                @endif
 
-        <div class="px-12 w-1/3">
-            <h1 class="text-3xl font-light mb-3">
-                @lang('The rabblings of a Front-End Developer. Covering HTML, CSS, JavaScript and PHP.')
-            </h1>
+                                <h2 class="mb-4 text-3xl text-white font-black">
+                                    {{ $featuredArticle->title }}
+                                </h2>
+
+                                @if ($featuredArticle->published_at)
+                                    <time datetime="{{ $featuredArticle->published_at }}"
+                                        aria-label="@lang('Posted On')"
+                                        class="block mb-6 text-base text-gray-500"
+                                    >
+                                        @date($featuredArticle->published_at)
+                                    </time>
+                                @endif
+
+                                @if ($featuredArticle->excerpt)
+                                    <p class="text-xl text-gray-200 font-light">
+                                        {!! $featuredArticle->excerpt !!}
+                                    </p>
+                                @endif
+                            </div>
+
+                            @if ($featuredArticle->isNew())
+                                <div class="absolute top-0 right-0 flex items-center justify-center bg-yellow-400 w-16 h-16 font-medium"
+                                    title="@lang('This article was published recently')"
+                                >
+                                    New
+                                </div>
+                            @endif
+
+                            <div class="absolute right-0 bottom-0 flex items-center justify-center bg-gray-200 text-xl w-16 h-16
+                                group-hover:bg-yellow-400"
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 32 32"
+                                    class="w-6"
+                                >
+                                    <path d="M18 6l-1.43 1.39L24.15 15H3v2h21.15l-7.58 7.57L18 26l10-10L18 6z" />
+                                </svg>
+                            </div>
+                        </article>
+                    </a>
+                </div>
+
+                <div class="px-12 w-1/3">
+                    <h1 class="text-3xl font-light mb-3">
+                        @lang('The rabblings of a Front-End Developer. Covering HTML, CSS, JavaScript and PHP.')
+                    </h1>
+            </div>
         </div>
-    </div>
+    @endif
 
     <div class="flex flex-wrap border-t border-l border-gray-900 mb-12">
         @foreach ($articles as $article)
