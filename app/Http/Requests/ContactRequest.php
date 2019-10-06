7d2a8b1e7da0ca18173a0fcb9919c3e608444a85
Sam Wrigley <sam@samwrigley.co.uk>
2019-10-06 15:35:46 +0100

    Created contact form request

---
+++ app/Http/Requests/ContactRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ContactRequest extends FormRequest
+{
+    /**
+     * The key to be used for the view error bag.
+     *
+     * @var string
+     */
+    protected $errorBag = 'contact';
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'message' => 'required|string',
+        ];
+    }
+}
