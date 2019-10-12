e65d7785dd2ddd3c790693458e6f9cc7bc1d8525
Sam Wrigley <sam@samwrigley.co.uk>
2019-10-12 17:38:54 +0100

    Created `ContactsTableSeeder` and added to `DatabaseSeeder`

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
             ArticleCategoriesTableSeeder::class,
             ArticleSeriesTableSeeder::class,
             ArticleTableSeeder::class,
+            ContactsTableSeeder::class,
         ]);
     }
 }
