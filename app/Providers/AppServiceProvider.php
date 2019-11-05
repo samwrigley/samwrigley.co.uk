ee49deb976b9400db436b54dbd02540c1b39c5e5
Sam Wrigley <sam@samwrigley.co.uk>
2019-11-05 22:14:30 +0000

    Registered Dusk service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Providers\DuskServiceProvider;
 use App\Providers\TelescopeServiceProvider;
 use Illuminate\Support\ServiceProvider;
 
@@ -15,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         if ($this->app->isLocal()) {
+            $this->app->register(DuskServiceProvider::class);
             $this->app->register(TelescopeServiceProvider::class);
         }
     }
