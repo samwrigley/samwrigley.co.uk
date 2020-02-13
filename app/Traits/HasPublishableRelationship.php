e05acb5f5405752484fa29bf29aef793bcee1fb4
Sam Wrigley <sam@samwrigley.co.uk>
2020-02-13 22:28:48 +0000

    Reduced line length

--- app/Traits/HasPublishableRelationship.php
+++ app/Traits/HasPublishableRelationship.php
@@ -8,22 +8,31 @@ trait HasPublishableRelationship
 {
     public function scopeHasPublished(Builder $query, string $relation): Builder
     {
-        return $query->whereHas($relation, function (Builder $query): void {
-            $query->published();
-        });
+        return $query->whereHas(
+            $relation,
+            static function (Builder $query): void {
+                $query->published();
+            }
+        );
     }
 
     public function scopeHasScheduled(Builder $query, string $relation): Builder
     {
-        return $query->whereHas($relation, function (Builder $query): void {
-            $query->scheduled();
-        });
+        return $query->whereHas(
+            $relation,
+            static function (Builder $query): void {
+                $query->scheduled();
+            }
+        );
     }
 
     public function scopeHasDraft(Builder $query, string $relation): Builder
     {
-        return $query->whereHas($relation, function (Builder $query): void {
-            $query->draft();
-        });
+        return $query->whereHas(
+            $relation,
+            static function (Builder $query): void {
+                $query->draft();
+            }
+        );
     }
 }
