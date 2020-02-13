04542a10c3e057e51c2f22f152078ca697111b9f
Sam Wrigley <sam@samwrigley.co.uk>
2020-02-13 22:28:02 +0000

    Improved route type hinting

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::get('about', static function (): View {
     return view('pages.about');
 })->name('about');
 
-Route::get('contact', function () {
+Route::get('contact', static function (): View {
     return view('pages.contact');
 })->name('contact');
 
