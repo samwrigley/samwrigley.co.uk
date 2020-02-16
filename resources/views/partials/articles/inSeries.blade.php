24c9ee58f2610257bae8719e2b9a8f7afc577a62
Sam Wrigley <sam@samwrigley.co.uk>
2020-02-16 14:15:45 +0000

    Improved article in-series notice styling

--- resources/views/partials/articles/inSeries.blade.php
+++ resources/views/partials/articles/inSeries.blade.php
@@ -1,18 +1,23 @@
 @if ($article->series && $article->series->articles->count())
-    <div class="text-xl">
-        <p class="mb-4">
+    <div class="text-xl py-8 border-t border-b border-gray-200">
+        <p class="mb-4 text-sm uppercase tracking-widest text-gray-600">
             @lang('article.in_series', ['count' => $article->series->articles->count()])
         </p>
         <ul>
             @foreach ($article->series->articles as $seriesArticle)
                 <li class="mb-1 last-child:mb-0">
-                    <span class="mr-1 text-gray-600">
-                        @lang('Part') {{ $loop->index + 1}}:
-                    </span>
                     @if ($seriesArticle->id === $article->id)
-                        {{ $seriesArticle->title }}
+                        <span class="text-gray-600">
+                            <span class="mr-1 font-semibold">
+                                @lang('Part') {{ $loop->index + 1}}:
+                            </span>
+                            {{ $seriesArticle->title }}
+                        </span>
                     @else
-                        <a href="{{ $seriesArticle->showPath() }}">
+                        <a href="{{ $seriesArticle->showPath() }}" class="hover:text-gray-600">
+                            <span class="mr-1 font-semibold">
+                                @lang('Part') {{ $loop->index + 1}}:
+                            </span>
                             {{ $seriesArticle->title }}
                         </a>
                     @endif
