dec4a55c0314733d67878c140c36af2edeec01bf
Sam Wrigley <sam@samwrigley.co.uk>
2020-02-16 13:52:11 +0000

    Extracted article in-series markup into a dedicated Blade partial

--- resources/views/layouts/article.blade.php
+++ resources/views/layouts/article.blade.php
@@ -70,32 +70,7 @@
                 </div>
             @endif
 
-            @if ($article->series && $article->series->articles->count())
-                <div class="text-xl">
-                    <p class="mb-4">
-                        @lang('article.in_series', ['count' => $article->series->articles->count()])
-                    </p>
-                    <ul>
-                        @foreach ($article->series->articles as $seriesArticle)
-                            <li class="mb-1 last-child:mb-0">
-                                <span class="mr-1 text-gray-600">
-                                    @lang('Part') {{ $loop->index + 1}}:
-                                </span>
-                                @if ($seriesArticle->showPath() === $article->showPath())
-                                    {{ $seriesArticle->title }}
-                                @else
-                                    <a href="{{ $seriesArticle->showPath() }}"
-                                        title="@lang("Read $seriesArticle->title")"
-                                        aria-label="@lang("Read $seriesArticle->title")"
-                                    >
-                                        {{ $seriesArticle->title }}
-                                    </a>
-                                @endif
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
+            @include('partials.articles.inSeries')
         </header>
 
         <div class="mx-auto max-w-4xl mb-32">
