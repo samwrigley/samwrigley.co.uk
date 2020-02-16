0a6dbf8d78837461e440e95bbf5babd86b5f58c0
Sam Wrigley <sam@samwrigley.co.uk>
2020-02-16 17:15:06 +0000

    Improved configuration file formatting

--- config/meta.php
+++ config/meta.php
@@ -1,7 +1,12 @@
 <?php
 
-return [
+/*
+|--------------------------------------------------------------------------
+| Meta Information
+|--------------------------------------------------------------------------
+*/
 
+return [
     'tagline' => 'Web-Developer & Graphic Designer',
     'description' => "Hello, I'm Sam Wrigley. I'm a Web-Developer and Graphic Designer.",
 ];
