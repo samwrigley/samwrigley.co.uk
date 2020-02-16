e27b06fb0a7d54e46573613f3702e9274fa627c5
Sam Wrigley <sam@samwrigley.co.uk>
2020-02-16 12:23:22 +0000

    Removed unused pages from main navigation

--- resources/views/partials/general/header.blade.php
+++ resources/views/partials/general/header.blade.php
@@ -5,14 +5,6 @@
 <nav>
     <ul class="flex">
         <li class="mr-4">
-            <a href="{{ route('about') }}">Work</a>
-        </li>
-
-        <li class="mr-4">
-            <a href="{{ route('services') }}">Services</a>
-        </li>
-
-        <li class="mr-4">
             <a href="{{ route('blog.articles.index') }}">Blog</a>
         </li>
 
