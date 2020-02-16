e33aebc5349b6a8b66a08c7c1637c03a44bcacf2
Sam Wrigley <sam@samwrigley.co.uk>
2020-02-16 13:34:09 +0000

    Moved out-of-date string from newsletter language file to article

--- resources/lang/en/article.php
+++ resources/lang/en/article.php
@@ -3,5 +3,6 @@
 return [
 
     'in_series' => 'This article is part of a :count part series.',
+    'out_of_date' => 'This article was published :date and likely out-of-date.',
 
 ];
--- resources/lang/en/newsletter.php
+++ resources/lang/en/newsletter.php
@@ -3,6 +3,5 @@
 return [
 
     'success' => 'Thank you for subscribing!',
-    'out_of_date' => 'This article was published :date and likely out-of-date.',
 
 ];
