88ed804c8c4d875da36247b73c56e55138b1b2bc
Sam Wrigley <sam@samwrigley.co.uk>
2020-03-07 12:30:26 +0000

    Apply fixes from StyleCI

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,10 @@ use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 use Illuminate\View\View;
 
-Route::get('/', static fn (): RedirectResponse
-    => Redirect::route('blog.articles.index'))->name('home');
+Route::get('/', static fn (): RedirectResponse => Redirect::route('blog.articles.index'))->name('home');
 
-Route::get('about', static fn (): View
-    => view('pages.about'))->name('about');
+Route::get('about', static fn (): View => view('pages.about'))->name('about');
 
-Route::get('contact', static fn (): View
-    => view('pages.contact'))->name('contact');
+Route::get('contact', static fn (): View => view('pages.contact'))->name('contact');
 
 Route::feeds();
