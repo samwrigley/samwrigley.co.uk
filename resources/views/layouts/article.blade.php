8617f0837e115a52dbed6f26cd251326c67ef7a5
Sam Wrigley <sam@samwrigley.co.uk>
2020-03-08 16:49:49 +0000

    Added responsive styling for `article` Blade layout

--- resources/views/layouts/article.blade.php
+++ resources/views/layouts/article.blade.php
@@ -1,36 +1,37 @@
 @extends('layouts.default')
 
 @section('content')
-    <article class="pt-20">
-        <header class="relative mx-auto max-w-4xl mb-16">
+    <article class="pt-12 sm:pt-20">
+        <header class="relative mx-auto max-w-4xl pb-4 mb-12 border-b border-black sm:mb-16">
             @include('partials.articles.outOfDate')
             @include('partials.articles.categories')
 
-            <h1 class="mb-4 text-6xl font-black leading-tight">
+            <h1 class="mb-4 text-3xl font-black leading-tight sm:text-6xl">
                 <a href="{{ $article->showPath() }}">
                     {{ $article->title }}
                 </a>
             </h1>
 
+            @include('partials.articles.author')
+
             @if ($article->published_at)
                 <time datetime="{{ $article->published_at }}"
                     aria-label="@lang('Posted On')"
-                    class="block mb-10 text-xl text-gray-600 leading-none"
+                    class="block text-base text-gray-600 leading-none sm:text-xl"
                     title="{{ $article->published_at }}"
                 >
                     @date($article->published_at)
                 </time>
             @endif
 
-            @include('partials.articles.author')
             @include('partials.articles.inSeries')
         </header>
 
-        <div class="mx-auto max-w-4xl mb-32">
+        <div class="mx-auto max-w-4xl mb-12 sm:mb-32">
             @yield('body')
         </div>
 
-        <footer class="flex border border-black">
+        <footer class="flex flex-col border border-black sm:flex-row">
             @include('partials.articles.info')
             @include('partials.articles.newsletter')
         </footer>
