54dbd35c9119933a06b33580bf79a18320cb2779
Sam Wrigley <sam@samwrigley.co.uk>
2020-03-15 14:14:10 +0000

    Added responsive styling for `newsletter` partial

--- resources/views/partials/articles/newsletter.blade.php
+++ resources/views/partials/articles/newsletter.blade.php
@@ -1,9 +1,9 @@
-<div class="flex-1 p-6 sm:p-16">
-    <h3 class="mb-2 text-2xl sm:text-4xl">
+<div class="flex-1 p-6 lg:p-16">
+    <h3 class="mb-2 text-2xl lg:text-4xl">
         Want to read more articles like this?
     </h3>
 
-    <label class="block mb-8 text-base text-gray-600 sm:text-2xl" id="emailLabel">
+    <label class="block mb-8 text-base text-gray-600 lg:text-2xl" id="emailLabel">
         Subscribe to my newsletter and you'll be the first to know.
     </label>
 
@@ -17,7 +17,7 @@
             required
             aria-describedby="emailLabel"
             autocomplete="email"
-            class="flex-1 p-4 text-base bg-gray-200 focus:outline-none sm:text-xl sm:px-6"
+            class="flex-1 p-4 text-base bg-gray-200 focus:outline-none lg:text-xl lg:px-6"
         >
 
         <button
@@ -29,7 +29,7 @@
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 32 32"
-                class="w-4 fill-current sm:w-6"
+                class="w-4 fill-current lg:w-6"
             >
                 <path d="M18 6l-1.43 1.39L24.15 15H3v2h21.15l-7.58 7.57L18 26l10-10L18 6z" />
             </svg>
