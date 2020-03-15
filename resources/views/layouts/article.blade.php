e66f63bd9d36af3a61903fdd9f8f2ee65bdb5c70
Sam Wrigley <sam@samwrigley.co.uk>
2020-03-15 15:21:18 +0000

    Improve responsive styling for `article` layout

--- resources/views/layouts/article.blade.php
+++ resources/views/layouts/article.blade.php
@@ -2,9 +2,22 @@
 
 @section('content')
     <article class="pt-12 sm:pt-20">
-        <header class="relative mx-auto max-w-4xl pb-4 mb-12 border-b border-black sm:mb-16">
+        <header class="relative mx-auto max-w-4xl mb-12 sm:mb-20">
             @include('partials.articles.outOfDate')
-            @include('partials.articles.categories')
+
+            <div class="flex flex-col mb-4 text-xs uppercase tracking-widest sm:flex-row sm:text-sm">
+                @if ($article->published_at)
+                    <time datetime="{{ $article->published_at }}"
+                        aria-label="@lang('Posted On')"
+                        class="mb-2 sm:mr-6 sm:mb-0"
+                        title="Published at {{ $article->published_at }}"
+                    >
+                        @date($article->published_at)
+                    </time>
+                @endif
+
+                @include('partials.articles.categories')
+            </div>
 
             <h1 class="mb-4 text-3xl font-black leading-tight md:text-4xl lg:text-6xl">
                 <a href="{{ $article->showPath() }}">
@@ -13,17 +26,6 @@
             </h1>
 
             @include('partials.articles.author')
-
-            @if ($article->published_at)
-                <time datetime="{{ $article->published_at }}"
-                    aria-label="@lang('Posted On')"
-                    class="block text-base text-gray-600 leading-none sm:text-xl"
-                    title="{{ $article->published_at }}"
-                >
-                    @date($article->published_at)
-                </time>
-            @endif
-
             @include('partials.articles.inSeries')
         </header>
 
