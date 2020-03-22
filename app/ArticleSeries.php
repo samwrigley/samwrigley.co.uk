827373ef8fff0398cee436cf72f7ce84d5c45a7f
Sam Wrigley <sam@samwrigley.co.uk>
2020-03-22 17:55:23 +0000

    Added `HasArticles` and `HasPublishableRelationship` traits to `ArticleSeries` model

--- app/ArticleSeries.php
+++ app/ArticleSeries.php
@@ -4,10 +4,15 @@ namespace App;
 
 use App\Article;
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\HasArticles;
+use App\Traits\HasPublishableRelationship;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ArticleSeries extends Model
 {
+    use HasArticles;
+    use HasPublishableRelationship;
+
     public function articles(): HasMany
     {
         return $this->hasMany(Article::class)
