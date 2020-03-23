5abbbbd5ba046cfa7f7f846ba35818b8d216287a
Sam Wrigley <sam@samwrigley.co.uk>
2020-03-23 21:47:40 +0000

    Created article series index tests

---
+++ tests/Feature/ArticleSeries/ArticleSeriesIndexTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Tests\Feature\Article;
+
+use App\Article;
+use App\ArticleSeries;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\TestResponse;
+use Tests\TestCase;
+
+class ArticleSeriesIndexTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function can_see_a_list_of_categories(): void
+    {
+        $series = factory(ArticleSeries::class)->create();
+        $articles = factory(Article::class, 5)->create();
+
+        $series->articles()->saveMany($articles);
+
+        $this->getSeriesIndexRoute()
+            ->assertOk()
+            ->assertSee($series->title);
+    }
+
+    /** @test */
+    public function cannot_see_a_series_that_has_no_articles(): void
+    {
+        $series = factory(ArticleSeries::class)->create();
+
+        $this->getSeriesIndexRoute()
+            ->assertOk()
+            ->assertDontSee($series->title);
+    }
+
+    /** @test */
+    public function cannot_see_a_series_that_only_has_scheduled_articles(): void
+    {
+        $series = factory(ArticleSeries::class)->create();
+        $articles = factory(Article::class, 2)->create([
+            'published_at' => now()->addDays(7),
+        ]);
+
+        $series->articles()->saveMany($articles);
+
+        $this->getSeriesIndexRoute()
+            ->assertOk()
+            ->assertDontSee($series->title);
+    }
+
+    /** @test */
+    public function cannot_see_a_series_that_only_has_draft_articles(): void
+    {
+        $series = factory(ArticleSeries::class)->create();
+        $articles = factory(Article::class, 2)->create([
+            'published_at' => null,
+        ]);
+
+        $series->articles()->saveMany($articles);
+
+        $this->getSeriesIndexRoute()
+            ->assertOk()
+            ->assertDontSee($series->title);
+    }
+
+    private function getSeriesIndexRoute(): TestResponse
+    {
+        return $this->get(route('blog.series.index'));
+    }
+}
