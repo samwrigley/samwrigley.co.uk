5ff07f60765bfebe913ab7dc4b0ad8c28070be7f
Sam Wrigley <sam@samwrigley.co.uk>
2020-03-24 22:15:29 +0000

    Improve series show view responsive styling

--- resources/views/series/show.blade.php
+++ resources/views/series/show.blade.php
@@ -3,55 +3,55 @@
 @section('title', $series->title)
 
 @section('content')
-    <div class="flex flex-wrap border-t border-l border-gray-900 mb-12">
+    <div class="grid mb-6 sm:mb-12">
         @foreach ($articles as $article)
             <a href="{{ $article->showPath() }}"
-                aria-label="Read {{ $article->title }}"
-                title="Read {{ $article->title }}"
-                class="block group relative w-1/3 border-r border-b border-gray-900 p-12 pb-32 hover:bg-gray-100"
+                aria-label="@lang('Read') {{ $article->title }}"
+                title="@lang('Read') {{ $article->title }}"
+                class="block group relative p-6 pb-16 hover:bg-gray-100 sm:p-12 sm:pb-32"
             >
                 <article>
                     <header class="mb-6">
-                        <div class="flex items-center mb-12">
+                        <div class="flex items-center mb-6 sm:mb-12">
                             @if ($article->categories->count())
-                                <ul class="text-sm text-gray-600 uppercase tracking-widest">
-                                    @foreach ($article->categories as $series)
+                                <ul class="text-xs text-gray-600 uppercase tracking-widest sm:text-sm">
+                                    @foreach ($article->categories as $category)
                                         <li class="leading-none">
-                                            {{ $series->name }}
+                                            {{ $category->name }}
                                         </li>
                                     @endforeach
                                 </ul>
                             @endif
 
                             @if ($article->isNew())
-                                <div class="absolute top-0 right-0 flex items-center justify-center bg-yellow-400 w-16 h-16 font-medium"
+                                <div class="absolute top-0 right-0 flex items-center justify-center bg-yellow-400 w-12 h-12 font-medium text-sm sm:w-16 sm:h-16 sm:text-base"
                                     title="@lang('This article was published recently')"
                                 >
                                     New
                                 </div>
                             @endif
 
-                            <div class="absolute right-0 bottom-0 flex items-center justify-center bg-gray-200 text-xl w-16 h-16
-                                group-hover:bg-gray-900 group-hover:text-white"
+                            <div class="absolute right-0 bottom-0 flex items-center justify-center bg-gray-200 text-xl w-12 h-12
+                                group-hover:bg-gray-900 group-hover:text-white sm:w-16 sm:h-16"
                             >
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     viewBox="0 0 32 32"
-                                    class="w-6 fill-current"
+                                    class="w-4 fill-current sm:w-6"
                                 >
                                     <path d="M18 6l-1.43 1.39L24.15 15H3v2h21.15l-7.58 7.57L18 26l10-10L18 6z" />
                                 </svg>
                             </div>
                         </div>
 
-                        <h2 class="mb-2 text-4xl font-bold leading-tight">
+                        <h2 class="mb-2 text-2xl leading-tight font-bold sm:text-4xl">
                             {{ $article->title }}
                         </h2>
 
                         @if ($article->published_at)
                             <time datetime="{{ $article->published_at }}"
                                 aria-label="@lang('Posted On')"
-                                class="text-base text-gray-600"
+                                class="text-sm text-gray-600 sm:text-base"
                             >
                                 @date($article->published_at)
                             </time>
@@ -59,7 +59,7 @@
                     </header>
 
                     @if ($article->excerpt)
-                        <p class="text-xl">
+                        <p class="text-base sm:text-xl">
                             {!! $article->excerpt !!}
                         </p>
                     @endif
