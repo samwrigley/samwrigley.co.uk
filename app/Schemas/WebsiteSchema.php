f23decd3a81cc960f53c384bcc548dbfbf742005
Sam Wrigley <sam@samwrigley.co.uk>
2020-03-30 21:41:17 +0100

    Created `WebsiteSchema`

---
+++ app/Schemas/WebsiteSchema.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Schemas;
+
+use App\Schemas\Contracts\SchemaContract;
+use Illuminate\Support\Facades\Config;
+use Spatie\SchemaOrg\Schema;
+use Spatie\SchemaOrg\WebSite;
+
+class WebsiteSchema implements SchemaContract
+{
+    public function generate(): WebSite
+    {
+        return Schema::website()
+            ->url(Config::get('app.url'))
+            ->name(Config::get('app.name'))
+            ->author(Schema::person()->name('Sam Wrigley'))
+            ->description(Config::get('meta.description'));
+    }
+}
