55efa5c666ff0f077ef09c56d484d2b7209d2615
Sam Wrigley <sam@samwrigley.co.uk>
2020-03-30 21:47:31 +0100

    Replaced hardcoded site schema with schema generated by `SiteSchema`

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -2,11 +2,16 @@
 
 namespace App\Providers;
 
+use App\Schemas\SiteSchema;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class ViewServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        $siteSchema = new SiteSchema();
+
+        View::share('siteSchema', $siteSchema->generate());
     }
 }
