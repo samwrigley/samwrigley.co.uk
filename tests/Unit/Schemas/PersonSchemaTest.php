add7d2467ba57efd9ebebb7bcec717c7393407cd
Sam Wrigley <sam@samwrigley.co.uk>
2020-03-31 20:32:19 +0100

    Created basic `PersonBusinessSchemaTest`

--- tests/Unit/Schemas/PersonSchemaTest.php
+++ tests/Unit/Schemas/PersonSchemaTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\Schemas;
 
 use App\Schemas\PersonSchema;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Config;
 use Spatie\SchemaOrg\Person;
 use Tests\TestCase;
 
@@ -17,5 +18,10 @@ class PersonSchemaTest extends TestCase
         $personSchema = (new PersonSchema())->generate();
 
         $this->assertInstanceOf(Person::class, $personSchema);
+        $this->assertEquals($personSchema['email'], Config::get('contact.email'));
+        $this->assertEquals($personSchema['image'], asset('images/sam-wrigley.png'));
+        $this->assertEquals($personSchema['name'], Config::get('app.name'));
+        $this->assertEquals($personSchema['telephone'], Config::get('contact.telephone'));
+        $this->assertEquals($personSchema['url'], Config::get('app.url'));
     }
 }
