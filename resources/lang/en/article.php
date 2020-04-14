2e2caa88427d7d659206a5e2f2cffc01dd62f796
Sam Wrigley <sam@samwrigley.co.uk>
2020-04-15 00:00:42 +0100

    Added article index page title to language file

--- resources/lang/en/article.php
+++ resources/lang/en/article.php
@@ -2,6 +2,7 @@
 
 return [
 
+    'index_page_title' => 'Blog',
     'in_series' => 'This article is in the :count part series',
     'out_of_date' => 'This article was published :date and likely out-of-date.',
 
