070311738335f97bcba3506284c2ffee93140023
Sam Wrigley <sam@samwrigley.co.uk>
2020-04-16 23:13:44 +0100

    Removed `highlight.js` NPM package as no longer needed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,1 @@
-import hljs from 'highlight.js/lib/highlight'
-import javascript from 'highlight.js/lib/languages/javascript'
-
 require('./bootstrap')
-
-hljs.registerLanguage('javascript', javascript)
-
-hljs.initHighlightingOnLoad()
-
