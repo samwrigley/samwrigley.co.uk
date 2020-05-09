71d60b3737e4723fa8032b007db35e40afd78b10
Sam Wrigley <sam@samwrigley.co.uk>
2020-05-09 09:58:09 +0100

    Refactored home route to allow route caching

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,8 @@
 <?php
 
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', static function (): RedirectResponse {
-    return Redirect::route('blog.articles.index');
-})->name('home');
-
+Route::redirect('/', '/blog')->name('home');
 Route::view('about', 'pages.about')->name('about');
 Route::view('contact', 'pages.contact')->name('contact');
 
