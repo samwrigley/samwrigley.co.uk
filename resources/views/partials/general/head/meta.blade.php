86c3429c72982b96760ab3c181625565e3c7aed7
Sam Wrigley <sam@samwrigley.co.uk>
2020-05-09 15:16:20 +0100

    Improved meta tags

--- resources/views/partials/general/head/meta.blade.php
+++ resources/views/partials/general/head/meta.blade.php
@@ -1,4 +1,6 @@
 <meta name="description" content="{{ config('meta.description') }}">
+<meta name="image" content="{{ asset('images/sam-wrigley.png') }}" />
+<meta name="keywords" content="Front End Engineer, Front End Developer, Web Developer, Software Engineer">
 
 {{-- Open Graph --}}
 <meta property="og:type" content="website" />
@@ -9,5 +11,8 @@
 <meta property="og:image" content="{{ asset('images/sam-wrigley.png') }}" />
 
 {{-- Twitter Cards --}}
+<meta name="twitter:title" content="{{ config('app.name') }} - {{ config('meta.tagline') }}" />
+<meta name="twitter:description" content="{{ config('meta.description') }}">
+<meta name="twitter:image" content="{{ asset('images/sam-wrigley.png') }}" />
+<meta name="twitter:creator" content="{{ '@' . config('social.twitter') }}">
 <meta name="twitter:card" content="summary_large_image">
-<meta name="twitter:site" content="{{ '@' . config('social.twitter') }}">
