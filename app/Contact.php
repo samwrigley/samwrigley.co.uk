9dfda072808e0be20dccf56174cbddb03b3899e4
Sam Wrigley <sam@samwrigley.co.uk>
2020-05-11 21:07:51 +0100

    Removed `Notifiable` trait and `routeNotificationForSlack` method as no longer needed

--- app/Contact.php
+++ app/Contact.php
@@ -3,12 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Notifications\Notifiable;
 
 class Contact extends Model
 {
-    use Notifiable;
-
     public const MAX_MESSAGE_LENGTH = 2000;
 
     /**
@@ -21,9 +18,4 @@ class Contact extends Model
         'email',
         'message',
     ];
-
-    public function routeNotificationForSlack(): string
-    {
-        return 'https://hooks.slack.com/services/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';
-    }
 }
