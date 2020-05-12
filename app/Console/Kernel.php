e480cc4b3b210e21360af51aae0d695dce8cf32c
Sam Wrigley <sam@samwrigley.co.uk>
2020-05-12 21:07:30 +0100

    Schedule sitemap to be generated daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
+        $schedule->command('sitemap:generate')->daily();
         $schedule->command('telescope:prune')->daily();
         $schedule->command('horizon:snapshot')->everyFiveMinutes();
     }
