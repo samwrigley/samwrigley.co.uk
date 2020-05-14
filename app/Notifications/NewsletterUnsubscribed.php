b51a9e4d63c90abb0aafed8dceb0b65e0c057822
Sam Wrigley <sam@samwrigley.co.uk>
2020-05-14 20:26:39 +0100

    Created `NewsletterUnsubscribed` notification

---
+++ app/Notifications/NewsletterUnsubscribed.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\SlackMessage;
+use Illuminate\Notifications\Notification;
+
+class NewsletterUnsubscribed extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    private string $email;
+
+    public function __construct(string $email)
+    {
+        $this->email = $email;
+    }
+
+    public function via(): array
+    {
+        return ['slack'];
+    }
+
+    public function toSlack(): SlackMessage
+    {
+        return (new SlackMessage)
+            ->from(config('app.name'))
+            ->to('#newsletter')
+            ->info()
+            ->content("{$this->email} has unsubscribed");
+    }
+}
