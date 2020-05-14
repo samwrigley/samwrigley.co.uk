b3c491054113e3376096ba360b4ee4572f6079c6
Sam Wrigley <sam@samwrigley.co.uk>
2020-05-14 20:25:33 +0100

    Added Slack newsletter webhook URL environment variable

--- config/notifications.php
+++ config/notifications.php
@@ -5,6 +5,7 @@ return [
     'slack' => [
         'queue' => env('SLACK_QUEUE_WEBHOOK_URL'),
         'contact' => env('SLACK_CONTACT_WEBHOOK_URL'),
+        'newsletter' => env('SLACK_NEWSLETTER_WEBHOOK_URL'),
     ],
 
 ];
