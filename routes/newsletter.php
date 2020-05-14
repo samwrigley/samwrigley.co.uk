4396e9270e5c29b4f2cf2fb1425e2e81a081e0e9
Sam Wrigley <sam@samwrigley.co.uk>
2020-05-14 20:27:53 +0100

    Added newsletter unsubscribe route

--- routes/newsletter.php
+++ routes/newsletter.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\NewsletterController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'newsletter', 'as' => 'newsletter.'], static function (): void {
-    Route::post('subscribe', 'NewsletterController')->name('subscribe');
+    Route::post('subscribe', [NewsletterController::class, 'store'])->name('subscribe');
+    Route::post('unsubscribe', [NewsletterController::class, 'destroy'])->name('unsubscribe');
 });
