fb273069144983f04a2d11984041297f9b2faab4
Sam Wrigley <sam@samwrigley.co.uk>
2020-05-16 16:01:21 +0100

    Improve `MailChimpWebhookType` enum

--- app/Enums/MailChimpWebhookType.php
+++ app/Enums/MailChimpWebhookType.php
@@ -5,19 +5,19 @@ namespace App\Enums;
 use BenSampo\Enum\Enum;
 
 /**
+ * @method static static CAMPAIGN()
+ * @method static static CLEANED()
+ * @method static static PROFILE()
  * @method static static SUBSCRIBE()
  * @method static static UNSUBSCRIBE()
- * @method static static PROFILE()
- * @method static static UPDATEEMAIL()
- * @method static static CLEANED()
- * @method static static CAMPAIGN()
+ * @method static static UPDATE_MAIL()
  */
 final class MailChimpWebhookType extends Enum
 {
+    public const CAMPAIGN = 'campaign';
+    public const CLEANED = 'cleaned';
+    public const PROFILE = 'profile';
     public const SUBSCRIBE = 'subscribe';
     public const UNSUBSCRIBE = 'unsubscribe';
-    public const PROFILE = 'profile';
-    public const UPDATEEMAIL = 'upemail';
-    public const CLEANED = 'cleaned';
-    public const CAMPAIGN = 'campaign';
+    public const UPDATE_MAIL = 'upemail';
 }
