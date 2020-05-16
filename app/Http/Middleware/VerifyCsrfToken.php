d89d03e1b691f50df8a84bf6047c07378da21690
Sam Wrigley <sam@samwrigley.co.uk>
2020-05-16 14:56:55 +0100

    Created MailChimp webhook route and excluded it from the `VerifyCsrfToken` middleware

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -11,5 +11,7 @@ class VerifyCsrfToken extends Middleware
      *
      * @var array
      */
-    protected $except = [];
+    protected $except = [
+        'webhooks/newsletter',
+    ];
 }
--- routes/newsletter.php
+++ routes/newsletter.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'newsletter', 'as' => 'newsletter.'], static function (): void {
     Route::post('subscribe', 'NewsletterSubscriptionController')->name('subscribe');
 });
+
+Route::webhooks('webhooks/newsletter', 'mailchimp');
