66e728aa61907c4f37ddc44ae25cd8ee85ce712c
Sam Wrigley <sam@samwrigley.co.uk>
2020-05-18 19:00:48 +0100

    Refactored newsletter webhook route into a dedicated webhook routes file

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -27,6 +27,7 @@ class RouteServiceProvider extends ServiceProvider
         $this->mapBlogRoutes();
         $this->mapContactRoutes();
         $this->mapNewsletterRoutes();
+        $this->mapWebhookRoutes();
     }
 
     protected function mapWebRoutes(): void
@@ -55,5 +56,12 @@ class RouteServiceProvider extends ServiceProvider
         Route::middleware('web')
             ->namespace($this->namespace)
             ->group(base_path('routes/newsletter.php'));
+    }
+
+    protected function mapWebhookRoutes(): void
+    {
+        Route::middleware('web')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/webhook.php'));
     }
 }
--- routes/newsletter.php
+++ routes/newsletter.php
@@ -5,5 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'newsletter', 'as' => 'newsletter.'], static function (): void {
     Route::post('subscribe', 'NewsletterSubscriptionController')->name('subscribe');
 });
-
-Route::webhooks('webhooks/newsletter', 'mailchimp');
