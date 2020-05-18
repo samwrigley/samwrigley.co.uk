f73c951b164efdb3645f19624126bfb7eb2d213b
Sam Wrigley <sam@samwrigley.co.uk>
2020-05-18 22:45:25 +0100

    Created `UpdateNewsletterSubscriptionEmail` listener

---
+++ tests/Feature/Listeners/UpdateNewsletterSubscriptionEmailTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Feature\Listeners;
+
+use App\Enums\MailChimpWebhookType;
+use App\Events\NewsletterSubscriptionEmailUpdated;
+use App\Listeners\UpdateNewsletterSubscriptionEmail;
+use App\NewsletterSubscription;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Log;
+use Spatie\WebhookClient\Models\WebhookCall;
+use Tests\TestCase;
+use TiMacDonald\Log\LogFake;
+
+class UpdateNewsletterSubscriptionEmailTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        Log::swap(new LogFake);
+    }
+
+    /** @test */
+    public function it_updates_newsletter_subscription_email(): void
+    {
+        $oldEmail = $this->faker->email;
+        $newEmail = $this->faker->email;
+
+        $subscription = factory(NewsletterSubscription::class)
+            ->state('subscribed')
+            ->create(['email' => $oldEmail]);
+
+        $webhookCall = WebhookCall::create([
+            'name' => Config::get('webhook-client.names.newsletter'),
+            'payload' => [
+                'type' => MailChimpWebhookType::UPDATE_MAIL,
+                'data' => [
+                    'old_email' => $oldEmail,
+                    'new_email' => $newEmail,
+                ],
+            ],
+        ]);
+
+        Log::assertNothingLogged();
+        $this->assertDatabaseHas('newsletter_subscriptions', ['email' => $oldEmail]);
+
+        $listener = new UpdateNewsletterSubscriptionEmail;
+        $listener->handle(new NewsletterSubscriptionEmailUpdated($subscription, $webhookCall));
+
+        $this->assertDatabaseHas('newsletter_subscriptions', ['email' => $newEmail]);
+        $this->assertDatabaseMissing('newsletter_subscriptions', ['email' => $oldEmail]);
+
+        Log::assertLoggedMessage('info', 'Newsletter : Updated email');
+    }
+}
