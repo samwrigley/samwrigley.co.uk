0d12e626345863fe6015413c7ff14605464d1b71
Sam Wrigley <sam@samwrigley.co.uk>
2020-05-24 17:06:15 +0100

    Moved default text colour from `html` tag to `body` and add overflow-x hidden class to `body`

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}" class="text-gray-900">
+<html lang="{{ app()->getLocale() }}">
     <head>
         @include('partials.general.head')
     </head>
-    <body class="min-h-screen">
+    <body class="min-h-screen overflow-x-hidden text-gray-900">
         <div id="app" class="px-4 py-6 flex flex-col min-h-screen sm:p-8">
             <div class="flex items-end mb-8 text-base leading-none">
                 @include('partials.general.header')
