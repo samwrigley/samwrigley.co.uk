421379727e9d8a764acda0d08b57ed6f8115ebc0
Sam Wrigley <sam@samwrigley.co.uk>
2020-05-25 10:25:10 +0100

    Extended spacing and rotate Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,16 @@
 module.exports = {
+    theme: {
+        extend: {
+            spacing: {
+                '72': '18rem',
+                '80': '20rem',
+                '88': '22rem',
+            },
+            rotate: {
+                '4': '4deg',
+            },
+        },
+    },
     variants: {
         margin: ['responsive', 'first', 'last'],
         backgroundColor: ['responsive', 'hover', 'focus', 'group-hover'],
