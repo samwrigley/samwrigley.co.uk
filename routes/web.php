98b7b6a825dc4d820bfdb9518f023db85e706a11
Sam Wrigley <sam@samwrigley.co.uk>
2020-05-26 23:08:17 +0100

    Added basic test to ensure Tailwind view loads successfully

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::redirect('/', '/blog')->name('home');
 Route::view('about', 'pages.about')->name('about');
 Route::view('contact', 'pages.contact')->name('contact');
-Route::view('tailwind', 'pages.tailwind');
+Route::view('tailwind', 'pages.tailwind')->name('tailwind');
 
 Route::feeds();
