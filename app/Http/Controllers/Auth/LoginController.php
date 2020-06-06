0be4ec754d7eddb88dbba402336e9f2bf241a437
Sam Wrigley <sam@samwrigley.co.uk>
2020-06-06 16:29:13 +0100

    Created login controller

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+class LoginController extends Controller
+{
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/admin';
+
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+}
