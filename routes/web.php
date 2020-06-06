4ffcadffd153704a561699842a876f731edc1f4c
Sam Wrigley <sam@samwrigley.co.uk>
2020-06-06 16:34:08 +0100

    Added authentication routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+
+Auth::routes(['register' => false]);
 
 Route::redirect('/', '/blog')->name('home');
 Route::view('about', 'pages.about')->name('about');
