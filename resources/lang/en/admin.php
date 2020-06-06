a503b00ea21812f6ebc4a0bac84f970a0be66b49
Sam Wrigley <sam@samwrigley.co.uk>
2020-06-06 17:49:13 +0100

    Created admin lang file

---
+++ resources/lang/en/admin.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'articles' => [
+        'successfully_created' => 'Successfully created article',
+        'successfully_updated' => 'Successfully updated article',
+        'successfully_deleted' => 'Successfully deleted article',
+    ],
+
+];
