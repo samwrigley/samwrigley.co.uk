3ddc686833760ff95626b86dfe98ba19cee51032
Sam Wrigley <sam@samwrigley.co.uk>
2020-06-06 16:47:10 +0100

    Created reset password controller

---
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ResetsPasswords;
+
+class ResetPasswordController extends Controller
+{
+    use ResetsPasswords;
+
+    /**
+     * Where to redirect users after resetting their password.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/admin';
+}
