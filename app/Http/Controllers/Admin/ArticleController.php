58dccf658b8b07e2b6affc94024b9b7d9c02bf76
Sam Wrigley <sam@samwrigley.co.uk>
2020-06-14 15:18:15 +0100

    Refactored admin article controller

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Article;
+use App\ArticleCategory;
+use App\ArticleSeries;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ArticleRequest;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
-use Illuminate\Support\Facades\Response;
 use Illuminate\View\View;
 
 class ArticleController extends Controller
@@ -23,42 +23,64 @@ class ArticleController extends Controller
 
     public function create(): View
     {
-        return view('admin.articles.create');
+        return view('admin.articles.create')->with([
+            'categories' => ArticleCategory::orderBy('name')->get(),
+            'series' => ArticleSeries::orderBy('title')->get(),
+        ]);
     }
 
     public function store(ArticleRequest $request): RedirectResponse
     {
-        $article = Auth::user()->articles()->create($request->validated());
+        $article = Auth::user()
+            ->articles()
+            ->create($request->all());
 
-        if ($request->filled(['date', 'time'])) {
-            $article->markAsScheduled(
-                Carbon::parse("{$request->date} {$request->time}")
-            );
+        if ($request->filled('categories')) {
+            $article->categories()->attach($request->categories);
         }
 
-        return Redirect::back()
-            ->with($request->errorBag, __('admin.articles.successfully_created'));
+        if ($request->filled('series')) {
+            $article->series()->associate($request->series);
+            $article->save();
+        }
+
+        return Redirect::back()->with(
+            'article',
+            __('admin.articles.successfully_created')
+        );
     }
 
     public function edit(Article $article): View
     {
-        return view('admin.articles.edit')->with('article', $article);
+        return view('admin.articles.edit')->with([
+            'article' => $article,
+            'categories' => ArticleCategory::orderBy('name')->get(),
+            'series' => ArticleSeries::orderBy('title')->get(),
+        ]);
     }
 
     public function update(Article $article, ArticleRequest $request): RedirectResponse
     {
-        $article->update($request->validated());
+        $article->update($request->all());
 
-        if ($request->filled(['date', 'time'])) {
-            $article->markAsScheduled(
-                Carbon::parse("{$request->date} {$request->time}")
-            );
-        } else {
-            $article->markAsDraft();
+        $article->categories()->detach();
+
+        $article->series()->dissociate();
+        $article->save();
+
+        if ($request->filled('categories')) {
+            $article->categories()->attach($request->categories);
         }
 
-        return Redirect::back()
-            ->with($request->errorBag, __('admin.articles.successfully_updated'));
+        if ($request->filled('series')) {
+            $article->series()->associate($request->series);
+            $article->save();
+        }
+
+        return Redirect::back()->with(
+            'article',
+            __('admin.articles.successfully_updated')
+        );
     }
 
     public function destroy(Article $article): RedirectResponse
