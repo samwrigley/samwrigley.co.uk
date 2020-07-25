27dba13dc7751649004b012a6c44ecf1016cefcf
Sam Wrigley <sam@samwrigley.co.uk>
2020-07-25 16:56:51 +0100

    Add admin article forbidden strings

--- resources/lang/en/admin.php
+++ resources/lang/en/admin.php
@@ -6,6 +6,8 @@ return [
         'successfully_created' => 'Successfully created article',
         'successfully_updated' => 'Successfully updated article',
         'successfully_deleted' => 'Successfully deleted article',
+        'forbidden_update' => "You're not unauthorized to update this article",
+        'forbidden_delete' => "You're not unauthorized to delete this article",
     ],
 
 ];
