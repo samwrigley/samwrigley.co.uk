01ef25262dad11b6d6e5893353bb23d305d087a4
Sam Wrigley <sam@samwrigley.co.uk>
2020-07-25 16:55:28 +0100

    Create article destroy tests

---
+++ tests/Feature/Admin/Article/ArticleDestroyTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature\Admin\Article;
+
+use App\Article;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ArticleDestroyTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function author_can_delete_article(): void
+    {
+        $user = factory(User::class)->create();
+        $article = $user->articles()->create(factory(Article::class)->make()->toArray());
+
+        $this->assertDatabaseCount('articles', 1);
+
+        $this->actingAs($user)
+            ->followingRedirects()
+            ->delete($article->destroyRoute())
+            ->assertOk()
+            ->assertViewIs('admin.articles.index')
+            ->assertSessionHasNoErrors();
+
+        $this->assertDatabaseCount('articles', 0);
+    }
+
+    /** @test */
+    public function non_author_cannot_delete_article(): void
+    {
+        $userOne = factory(User::class)->create();
+        $userTwo = factory(User::class)->create();
+        $article = $userTwo->articles()->create(factory(Article::class)->make()->toArray());
+
+        $this->assertDatabaseCount('articles', 1);
+
+        $this->actingAs($userOne)
+            ->followingRedirects()
+            ->from(route('admin.articles.edit', ['article' => $article]))
+            ->delete($article->destroyRoute())
+            ->assertOk()
+            ->assertViewIs('admin.articles.edit')
+            ->assertSeeText(__('admin.articles.forbidden_delete'))
+            ->assertSessionHasNoErrors();
+
+        $this->assertDatabaseCount('articles', 1);
+        $this->assertDatabaseHas('articles', $article->toArray());
+    }
+}
