ba8476644f7ea32647727a816a482cf0e240b85c
Sam Wrigley <sam@samwrigley.co.uk>
2020-08-09 16:34:42 +0100

    Add basic contact page Cypress tests

---
+++ cypress/strings.js
@@ -0,0 +1,9 @@
+// Contact Page
+export const CONTACT_PAGE_TITLE = 'Contact';
+export const CONTACT_PAGE_HEADING = 'Get in touch';
+
+// Contact Form
+export const CONTACT_FORM_NAME_FIELD_LABEL = "Let's start with your name";
+export const CONTACT_FORM_EMAIL_FIELD_LABEL = 'Your email, so I can get back to you';
+export const CONTACT_FORM_MESSAGE_FIELD_LABEL = 'How can I help you?';
+export const CONTACT_FORM_SUCCESS_MESSAGE = 'Thank you for getting in touch!';
