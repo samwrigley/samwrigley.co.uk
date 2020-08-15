3e5353514d252c4b64c0c5661e150b42e5e538f4
Sam Wrigley <sam@samwrigley.co.uk>
2020-08-15 11:39:58 +0100

    Install and configure Cypress snapshots plugin

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,1 +1,7 @@
-module.exports = () => {};
+const { initPlugin } = require('cypress-plugin-snapshots/plugin');
+
+module.exports = (on, config) => {
+    initPlugin(on, config);
+
+    return config;
+};
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,1 +1,2 @@
 import '@testing-library/cypress/add-commands';
+import 'cypress-plugin-snapshots/commands';
