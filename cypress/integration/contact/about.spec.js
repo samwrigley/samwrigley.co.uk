201aa62e7a9cbd02eb6fbc5bb3ebdf59a029fa59
Sam Wrigley <sam@samwrigley.co.uk>
2020-08-15 11:58:31 +0100

    Add mobile, tablet and desktop about page snapshot tests

---
+++ cypress/integration/contact/about.spec.js
@@ -0,0 +1,24 @@
+import { MOBILE_VIEWPORT, TABLET_VIEWPORT, DESKTOP_VIEWPORT } from '../../constants';
+
+const ABOUT_PAGE_PATH = 'about';
+
+describe('About', () => {
+    beforeEach(() => {
+        cy.visit(ABOUT_PAGE_PATH);
+    });
+
+    it('matches mobile screenshot', () => {
+        cy.viewport(MOBILE_VIEWPORT);
+        cy.document().toMatchImageSnapshot();
+    });
+
+    it('matches tablet screenshot', () => {
+        cy.viewport(TABLET_VIEWPORT);
+        cy.document().toMatchImageSnapshot();
+    });
+
+    it('matches desktop screenshot', () => {
+        cy.viewport(DESKTOP_VIEWPORT);
+        cy.document().toMatchImageSnapshot();
+    });
+});
