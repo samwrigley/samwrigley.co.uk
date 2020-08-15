94b394f57f9fbeff1d5a90bababc26de15bc5799
Sam Wrigley <sam@samwrigley.co.uk>
2020-08-15 12:15:58 +0100

    Add mobile, tablet and desktop blog series snapshot tests

--- cypress/constants.js
+++ cypress/constants.js
@@ -3,6 +3,7 @@ export const ABOUT_INDEX_PATH = 'about';
 export const CONTACT_INDEX_PATH = 'contact';
 export const BLOG_INDEX_PATH = 'blog';
 export const BLOG_CATEGORIES_INDEX_PATH = BLOG_INDEX_PATH + '/categories';
+export const BLOG_SERIES_INDEX_PATH = BLOG_INDEX_PATH + '/series';
 
 // Viewports
 export const MOBILE_VIEWPORT = 'iphone-6';
