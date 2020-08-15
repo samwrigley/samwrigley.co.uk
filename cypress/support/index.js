e27bfa9489b6ea49b8faa310005a9f2d4043a9c7
Sam Wrigley <sam@samwrigley.co.uk>
2020-08-15 17:22:13 +0100

    Publish `laracasts/cypress` boilerplate

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,1 +1,11 @@
 import './commands';
+import './laravel-commands';
+import './assertions';
+
+before(() => {
+    cy.task('activateCypressEnvFile', {}, { log: false });
+});
+
+after(() => {
+    cy.task('activateLocalEnvFile', {}, { log: false });
+});
