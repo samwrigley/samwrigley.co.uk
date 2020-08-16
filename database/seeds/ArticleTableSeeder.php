a5b07c57db286874acc253004918c405c4da8bf6
Sam Wrigley <sam@samwrigley.co.uk>
2020-08-16 17:31:24 +0100

    Ensure author and article body remain consistent on permanent test article

--- database/seeds/ArticleTableSeeder.php
+++ database/seeds/ArticleTableSeeder.php
@@ -2,6 +2,7 @@
 
 use App\Article;
 use App\ArticleCategory;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class ArticleTableSeeder extends Seeder
@@ -9,8 +10,10 @@ class ArticleTableSeeder extends Seeder
     public function run(): void
     {
         factory(Article::class)->states('published')->create([
+            'user_id' => factory(User::class)->create(['name' => 'Jane Doe']),
             'title' => 'Test',
             'slug' => 'test',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
         ]);
 
         factory(Article::class, 50)
