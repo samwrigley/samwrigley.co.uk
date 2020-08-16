607fa3dee822b3774b440002b60d3977749855c9
Sam Wrigley <sam@samwrigley.co.uk>
2020-08-16 12:15:12 +0100

    Add mobile, tablet and desktop blog article snapshot tests

---
+++ cypress/integration/blog/article.spec.js
@@ -0,0 +1,29 @@
+import {
+    BLOG_INDEX_PATH,
+    DESKTOP_VIEWPORT,
+    MOBILE_VIEWPORT,
+    TABLET_VIEWPORT,
+} from '../../constants';
+
+const TEST_ARTICLE_SHOW_PATH = BLOG_INDEX_PATH + 'test';
+
+describe.only('Blog Index', () => {
+    beforeEach(() => {
+        cy.visit(TEST_ARTICLE_SHOW_PATH);
+    });
+
+    it('matches mobile screenshot', () => {
+        cy.viewport(MOBILE_VIEWPORT);
+        cy.document().toMatchImageSnapshot();
+    });
+
+    it('matches tablet screenshot', () => {
+        cy.viewport(TABLET_VIEWPORT);
+        cy.document().toMatchImageSnapshot();
+    });
+
+    it('matches desktop screenshot', () => {
+        cy.viewport(DESKTOP_VIEWPORT);
+        cy.document().toMatchImageSnapshot();
+    });
+});
