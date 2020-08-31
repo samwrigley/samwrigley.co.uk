c96ed6f9496021b85d51b9f3477ab65fe148e63e
Sam Wrigley <sam@samwrigley.co.uk>
2020-08-31 16:00:34 +0100

    Add swap environment plugin

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,2 +1,10 @@
 import './commands';
 import './laravel-commands';
+
+before(() => {
+    cy.task('activateCypressEnvFile', {}, { log: false });
+});
+
+after(() => {
+    cy.task('activateLocalEnvFile', {}, { log: false });
+});
