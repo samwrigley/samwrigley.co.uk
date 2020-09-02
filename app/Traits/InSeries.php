88c9e52b14a339b951426af8254aa24f76a07fdc
Sam Wrigley <sam@samwrigley.co.uk>
2020-09-02 08:35:56 +0100

    Remove unused `saveSeries` method

--- app/Traits/InSeries.php
+++ app/Traits/InSeries.php
@@ -9,17 +9,6 @@ trait InSeries
 {
     abstract public function series(): BelongsTo;
 
-    public function saveSeries(array $series): void
-    {
-        if (empty($series)) {
-            return;
-        }
-
-        $this->series()->dissociate();
-        $this->series()->associate($series);
-        $this->save();
-    }
-
     public function scopeWithSeries(Builder $query): Builder
     {
         return $query->with('series');
