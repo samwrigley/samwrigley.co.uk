6782dd090671b10164c76736dfb2aaaa08ca857d
Sam Wrigley <sam@samwrigley.co.uk>
2020-09-03 20:31:39 +0100

    Refactored login submit icon to use Blade HeroIcons

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -48,13 +48,7 @@
                     class="flex items-center text-white bg-gray-900 py-3 px-4 rounded hover:bg-gray-800"
                 >
                     <span class="mr-2">{{ __('Login') }}</span>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 32 32"
-                        class="w-3 fill-current md:w-4"
-                    >
-                        <path d="M18 6l-1.43 1.39L24.15 15H3v2h21.15l-7.58 7.57L18 26l10-10L18 6z" />
-                    </svg>
+                    <x-heroicon-o-arrow-right class="w-3 fill-current md:w-4" />
                 </button>
             </form>
         </div>
