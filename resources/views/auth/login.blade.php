8c2e06aae447954fcc2be192cdd6e7a856f4124d
Sam Wrigley <sam@samwrigley.co.uk>
2020-09-06 15:39:22 +0100

    Created icon button Blade component

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -34,13 +34,7 @@
                     label="{{ __('Remember me') }}"
                 />
 
-                <button
-                    type="submit"
-                    class="flex items-center text-white bg-gray-900 py-3 px-4 rounded hover:bg-gray-800"
-                >
-                    <span class="mr-2">{{ __('Login') }}</span>
-                    <x-heroicon-o-arrow-right class="w-3 fill-current md:w-4" />
-                </button>
+                <x-admin.buttons.icon text="{{ __('Login') }}" />
             </x-form>
         </div>
     </div>
