08c55bedf37b552336b4a49733b2922579b6af19
Sam Wrigley <sam@samwrigley.co.uk>
2020-09-10 22:11:53 +0100

    Removed unused strings

--- cypress/strings.js
+++ cypress/strings.js
@@ -11,6 +11,3 @@ export const CONTACT_FORM_SUCCESS_MESSAGE = 'Thank you for getting in touch!';
 // Newsletter Form
 export const NEWSLETTER_FORM_FIELD_LABEL = 'Subscribe to my newsletter';
 export const NEWSLETTER_FORM_SUCCESS_MESSAGE = 'Thank you for subscribing!';
-export const NEWSLETTER_FORM_ALREADY_SUBSCRIBED_MESSAGE = "You're already subscribed.";
-export const NEWSLETTER_FORM_FAILURE_MESSAGE =
-    'It looks like something went wrong. Please try again or get in touch.';
