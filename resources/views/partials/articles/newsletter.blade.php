228fef28c519b03f0bf40493a3629f981fc39935
Sam Wrigley <sam@samwrigley.co.uk>
2020-09-10 22:12:12 +0100

    Corrected newsletter partial

--- resources/views/partials/articles/newsletter.blade.php
+++ resources/views/partials/articles/newsletter.blade.php
@@ -11,34 +11,36 @@
         @csrf
         @honeypot
 
-        <input
-            type="email"
-            name="email"
-            placeholder="Subscribe for updates"
-            required
-            aria-label="Subscribe to my newsletter"
-            autocomplete="email"
-            class="flex-1 p-4 text-base bg-gray-200 lg:text-xl lg:px-6"
-        >
-
-        <button
-            type="submit"
-            class="text-white bg-gray-900 py-4 px-6 hover:bg-gray-800"
-            title="Subscribe"
-            aria-label="Subscribe"
-        >
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 32 32"
-                class="w-4 fill-current lg:w-6"
+        <div class="flex">
+            <input
+                type="email"
+                name="email"
+                placeholder="Subscribe for updates"
+                required
+                aria-label="Subscribe to my newsletter"
+                autocomplete="email"
+                class="flex-1 p-4 text-base bg-gray-200 lg:text-xl lg:px-6"
             >
-                <path d="M18 6l-1.43 1.39L24.15 15H3v2h21.15l-7.58 7.57L18 26l10-10L18 6z" />
-            </svg>
-        </button>
-    </form>
 
-    <div class="mt-4 text-md">
-        @include('components.general.errors', ['errorBag' => 'newsletter'])
-        @include('components.general.session', ['key' => 'newsletter'])
-    </div>
+            <button
+                type="submit"
+                class="text-white bg-gray-900 py-4 px-6 hover:bg-gray-800"
+                title="Subscribe"
+                aria-label="Subscribe"
+            >
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 32 32"
+                    class="w-4 fill-current lg:w-6"
+                >
+                    <path d="M18 6l-1.43 1.39L24.15 15H3v2h21.15l-7.58 7.57L18 26l10-10L18 6z" />
+                </svg>
+            </button>
+        </div>
+
+        <div class="mt-4 text-md">
+            @include('components.general.errors', ['errorBag' => 'newsletter'])
+            @include('components.general.session', ['key' => 'newsletter'])
+        </div>
+    </form>
 </div>
