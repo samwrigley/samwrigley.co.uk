166f68d7a4f313f15f7838315439479267d18d40
Sam Wrigley <sam@samwrigley.co.uk>
2020-09-11 08:51:55 +0100

    Remove swap env task

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -2,10 +2,7 @@ import './commands';
 import './laravel-commands';
 
 before(() => {
-    cy.task('activateCypressEnvFile', {}, { log: false });
     cy.exec('php artisan key:generate');
 });
 
-after(() => {
-    cy.task('activateLocalEnvFile', {}, { log: false });
-});
+after(() => {});
