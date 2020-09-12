e9addd289f23876fa9697b559bbf437b2068288b
Sam Wrigley <sam@samwrigley.co.uk>
2020-09-12 18:05:30 +0100

    Improve formatting

--- database/seeds/ArticleTableSeeder.php
+++ database/seeds/ArticleTableSeeder.php
@@ -9,12 +9,14 @@ class ArticleTableSeeder extends Seeder
 {
     public function run(): void
     {
-        factory(Article::class)->states('published')->create([
-            'user_id' => factory(User::class)->create(['name' => 'Jane Doe']),
-            'title' => 'Test',
-            'slug' => 'test',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-        ]);
+        factory(Article::class)
+            ->states('published')
+            ->create([
+                'user_id' => factory(User::class)->create(['name' => 'Jane Doe']),
+                'title' => 'Test',
+                'slug' => 'test',
+                'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            ]);
 
         factory(Article::class, 50)
             ->create()
