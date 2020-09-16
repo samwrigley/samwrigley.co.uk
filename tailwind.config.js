1fe0365d243898dc7215f704a7b59acf93597bf1
Sam Wrigley <sam@samwrigley.co.uk>
2020-09-16 19:55:43 +0100

    Correct linting

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@ module.exports = {
     theme: {
         extend: {
             spacing: {
-                '72': '18rem',
-                '80': '20rem',
-                '88': '22rem',
+                72: '18rem',
+                80: '20rem',
+                88: '22rem',
             },
             fontSize: {
                 '8xl': '6rem',
@@ -14,7 +14,7 @@ module.exports = {
                 '26xl': '24rem',
             },
             rotate: {
-                '4': '4deg',
+                4: '4deg',
             },
             screens: {
                 xxl: '1600px',
