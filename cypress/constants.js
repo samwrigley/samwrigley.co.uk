62a5d3d40122ac055e3251e9c1232668210a21fe
Sam Wrigley <sam@samwrigley.co.uk>
2020-09-19 16:36:14 +0100

    Update Cypress constants

--- cypress/constants.js
+++ cypress/constants.js
@@ -1,15 +1,18 @@
 // Paths
 export const ABOUT_INDEX_PATH = 'about';
 export const CONTACT_INDEX_PATH = 'contact';
-export const BLOG_INDEX_PATH = 'blog/';
-export const BLOG_CATEGORIES_INDEX_PATH = BLOG_INDEX_PATH + 'categories/';
-export const BLOG_SERIES_INDEX_PATH = BLOG_INDEX_PATH + 'series/';
+export const BLOG_INDEX_PATH = 'blog';
+export const BLOG_CATEGORIES_INDEX_PATH = BLOG_INDEX_PATH + '/categories';
+export const BLOG_SERIES_INDEX_PATH = BLOG_INDEX_PATH + '/series';
+
+// Auth Paths
+export const LOGIN_PATH = 'login';
 
 // Admin Paths
-export const ADMIN_DASHBOARD_PATH = 'admin/';
-export const ADMIN_ARTICLE_INDEX_PATH = ADMIN_DASHBOARD_PATH + 'articles/';
-export const ADMIN_ARTICLE_CREATE_PATH = ADMIN_ARTICLE_INDEX_PATH + 'create';
-export const ADMIN_ARTICLE_EDIT_PATH = ADMIN_ARTICLE_INDEX_PATH + '{article}/edit';
+export const ADMIN_DASHBOARD_PATH = 'admin';
+export const ADMIN_ARTICLE_INDEX_PATH = ADMIN_DASHBOARD_PATH + '/articles';
+export const ADMIN_ARTICLE_CREATE_PATH = ADMIN_ARTICLE_INDEX_PATH + '/create';
+export const ADMIN_ARTICLE_EDIT_PATH = ADMIN_ARTICLE_INDEX_PATH + '/{article}/edit';
 
 // Viewports
 export const MOBILE_VIEWPORT = 'iphone-6';
