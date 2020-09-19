2af294b82218e24e76ef8ea6b7fcb656254f1dab
Sam Wrigley <sam@samwrigley.co.uk>
2020-09-19 16:36:34 +0100

    Update Cypress strings

--- cypress/strings.js
+++ cypress/strings.js
@@ -1,13 +1,45 @@
-// Contact Page
-export const CONTACT_PAGE_TITLE = 'Contact';
-export const CONTACT_PAGE_HEADING = 'Get in touch';
-
-// Contact Form
+// Contact
+export const CONTACT_VIEW_TITLE = 'Contact';
+export const CONTACT_VIEW_HEADING = 'Get in touch';
 export const CONTACT_FORM_NAME_FIELD_LABEL = "Let's start with your name";
 export const CONTACT_FORM_EMAIL_FIELD_LABEL = 'Your email, so I can get back to you';
 export const CONTACT_FORM_MESSAGE_FIELD_LABEL = 'How can I help you?';
 export const CONTACT_FORM_SUCCESS_MESSAGE = 'Thank you for getting in touch!';
 
-// Newsletter Form
+// About
+export const ABOUT_VIEW_TITLE = 'About';
+export const ABOUT_VIEW_HEADING = 'About';
+
+// Blog
+export const BLOG_INDEX_VIEW_TITLE = 'Blog';
+export const BLOG_CATEGORIES_INDEX_VIEW_TITLE = 'Categories';
+export const BLOG_SERIES_INDEX_VIEW_TITLE = 'Series';
+export const ARTICLE_SHOW_VIEW_TITLE = 'Test';
+export const ARTICLE_SHOW_VIEW_HEADING = 'Test';
+
+// Newsletter
 export const NEWSLETTER_FORM_FIELD_LABEL = 'Subscribe to my newsletter';
 export const NEWSLETTER_FORM_SUCCESS_MESSAGE = 'Thank you for subscribing!';
+
+// Auth
+export const LOGIN_VIEW_TITLE = 'Login';
+export const LOGIN_VIEW_HEADING = 'Login';
+export const LOGIN_FORM_EMAIL_FIELD_LABEL = 'Email';
+export const LOGIN_FORM_PASSWORD_FIELD_LABEL = 'Password';
+export const LOGIN_FORM_INVALID_MESSAGE = 'These credentials do not match our records.';
+
+// Dashboard
+export const DASHBOARD_VIEW_TITLE = 'Dashboard';
+export const DASHBOARD_VIEW_HEADING = 'Dashboard';
+
+// Admin Article Index
+export const ADMIN_ARTICLE_INDEX_VIEW_TITLE = 'All articles';
+export const ADMIN_ARTICLE_INDEX_VIEW_HEADING = 'All articles';
+
+// Admin Article Create
+export const ADMIN_ARTICLE_CREATE_VIEW_TITLE = 'Create article';
+export const ADMIN_ARTICLE_CREATE_VIEW_HEADING = 'Create article';
+
+// Admin Article Edit
+export const ADMIN_ARTICLE_EDIT_VIEW_TITLE = 'Edit Test';
+export const ADMIN_ARTICLE_EDIT_VIEW_HEADING = 'Edit article';
