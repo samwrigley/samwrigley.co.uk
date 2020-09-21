29d8032b45efa2aa8d547bb19982e70046015587
Sam Wrigley <sam@samwrigley.co.uk>
2020-09-21 13:52:57 +0100

    Create admin article index tests

--- cypress/integration/admin/article/index.spec.js
+++ cypress/integration/admin/article/index.spec.js
@@ -1,4 +1,10 @@
-import { ADMIN_ARTICLE_INDEX_PATH, DESKTOP_VIEWPORT } from '../../../constants';
+import * as strings from '../../../strings';
+import {
+    ADMIN_ARTICLE_EDIT_PATH,
+    ADMIN_ARTICLE_INDEX_PATH,
+    DESKTOP_VIEWPORT,
+    TEST_ARTICLE_ID,
+} from '../../../constants';
 
 describe('Admin Article Index', () => {
     beforeEach(() => {
@@ -9,5 +15,18 @@ describe('Admin Article Index', () => {
     it('matches desktop screenshot', () => {
         cy.viewport(DESKTOP_VIEWPORT);
         cy.document().toMatchImageSnapshot();
+    });
+
+    it('has page title', () => {
+        cy.title().should('contain', strings.ADMIN_ARTICLE_INDEX_VIEW_TITLE);
+    });
+
+    it('has h1 heading', () => {
+        cy.get('h1').should('contain', strings.ADMIN_ARTICLE_INDEX_VIEW_HEADING);
+    });
+
+    it('can navigate to article edit view', () => {
+        cy.findByText('Test').click();
+        cy.url().should('include', ADMIN_ARTICLE_EDIT_PATH.replace('{article}', TEST_ARTICLE_ID));
     });
 });
