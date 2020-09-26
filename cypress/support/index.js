92e791e85b71551a930874632c2c61123c60a2e9
Sam Wrigley <sam@samwrigley.co.uk>
2020-09-26 18:17:46 +0100

    Run refresh database command locally

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -6,6 +6,7 @@ const environment = Cypress.env('environment');
 before(() => {
     if (environment === 'local') {
         cy.task('swapCypressEnvFile', {}, { log: false });
+        cy.refreshDatabase({ '--seeder': 'DatabaseTestSeeder' });
     }
 });
 
