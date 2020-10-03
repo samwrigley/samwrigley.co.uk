72eb9f91970d6c44e7a2f3eb97958c489b2b637f
Sam Wrigley <sam@samwrigley.co.uk>
2020-10-03 16:19:56 +0100

    Improve CommonMark testing

--- tests/Unit/CommonMarkTest.php
+++ tests/Unit/CommonMarkTest.php
@@ -3,15 +3,37 @@
 namespace Tests\Unit;
 
 use App\Services\CommonMark\CommonMark;
+use Spatie\Snapshots\MatchesSnapshots;
 use Tests\TestCase;
 
 class CommonMarkTest extends TestCase
 {
+    use MatchesSnapshots;
+
     /** @test */
     public function it_converts_markdown_to_html(): void
     {
-        $html = CommonMark::convertToHtml('# Heading');
+        $markdown = <<<'MARKDOWN'
+        # Heading
+        Some text
+        MARKDOWN;
 
-        $this->assertEquals($html, "<h1>Heading</h1>\n");
+        $html = CommonMark::convertToHtml($markdown);
+
+        $this->assertMatchesXmlSnapshot("<div>{$html}</div>");
+    }
+
+    /** @test */
+    public function it_highlights_code_in_markdown(): void
+    {
+        $markdown = <<<'MARKDOWN'
+        ```js
+        const foo = 'bar';
+        ```
+        MARKDOWN;
+
+        $html = CommonMark::convertToHtml($markdown);
+
+        $this->assertMatchesXmlSnapshot("<div>{$html}</div>");
     }
 }
