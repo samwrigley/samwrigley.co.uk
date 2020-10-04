9be5e52ba7725bfa316f7ce5adeb5da1f68cd897
Sam Wrigley <sam@samwrigley.co.uk>
2020-10-04 12:40:17 +0100

    Updated content security policy

--- app/Services/Csp/Policy.php
+++ app/Services/Csp/Policy.php
@@ -3,30 +3,20 @@
 namespace App\Services\Csp;
 
 use Spatie\Csp\Directive;
+use Spatie\Csp\Policies\Basic;
 use Spatie\Csp\Policies\Policy as BasePolicy;
 
-class Policy extends BasePolicy
+class Policy extends Basic
 {
     public function configure(): void
     {
-        $this->addGeneralDirectives();
-        $this->addDirectivesImages();
-        $this->addDirectivesForFonts();
-        $this->addDirectivesForGoogleAnalytics();
-        $this->addDirectivesForGoogleTagManager();
+        parent::configure();
+
+        $this->addImageDirectives();
+        $this->addFontDirectives();
     }
 
-    protected function addGeneralDirectives(): BasePolicy
-    {
-        return $this
-            ->addDirective(Directive::DEFAULT, 'self')
-            ->addDirective(Directive::SCRIPT, 'self')
-            ->addDirective(Directive::STYLE, 'self')
-            ->addDirective(Directive::IMG, 'self')
-            ->addNonceForDirective(Directive::SCRIPT);
-    }
-
-    protected function addDirectivesImages(): BasePolicy
+    protected function addImageDirectives(): BasePolicy
     {
         return $this->addDirective(Directive::IMG, [
             '*.cloudinary.com',
@@ -34,23 +24,11 @@ class Policy extends BasePolicy
         ]);
     }
 
-    protected function addDirectivesForFonts(): BasePolicy
+    protected function addFontDirectives(): BasePolicy
     {
         return $this
             ->addDirective(Directive::FONT, 'fonts.gstatic.com')
             ->addDirective(Directive::SCRIPT, 'fonts.googleapis.com')
             ->addDirective(Directive::STYLE, 'fonts.googleapis.com');
-    }
-
-    protected function addDirectivesForGoogleAnalytics(): BasePolicy
-    {
-        return $this
-            ->addDirective(Directive::SCRIPT, '*.google-analytics.com')
-            ->addDirective(Directive::IMG, '*.google-analytics.com');
-    }
-
-    protected function addDirectivesForGoogleTagManager(): BasePolicy
-    {
-        return $this->addDirective(Directive::SCRIPT, '*.googletagmanager.com');
     }
 }
