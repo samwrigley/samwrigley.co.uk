fa20d2d9d57d9619e3c86956b960d7de0bd82424
Sam Wrigley <sam@samwrigley.co.uk>
2020-10-18 10:03:49 +0100

    Created utility to create fetch client

---
+++ resources/js/utilities/createClient.ts
@@ -0,0 +1,22 @@
+import getCsrfToken from './getCsrfToken';
+
+interface Body {
+    [key: string]: any;
+}
+
+interface Headers {
+    [key: string]: string;
+}
+
+export default function createClient() {
+    const csrfToken = getCsrfToken();
+    const headers: Headers = { 'Content-Type': 'application/json' };
+
+    if (csrfToken) headers['X-CSRF-TOKEN'] = csrfToken;
+
+    return async (url: string, body: Body, method: string = 'POST') => {
+        const response = await fetch(url, { method, headers, body: JSON.stringify(body) });
+
+        return await response.json();
+    };
+}
