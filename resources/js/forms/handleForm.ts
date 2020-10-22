0b75274a5983d0e274df5ce82816e1015ae86e95
Sam Wrigley <sam@samwrigley.co.uk>
2020-10-22 20:24:50 +0100

    Improve handleForm

--- resources/js/forms/handleForm.ts
+++ resources/js/forms/handleForm.ts
@@ -1,10 +1,12 @@
 import createClient, { Body } from '../utilities/createClient';
 
-const CLIENT = createClient();
+interface ResponseData {
+    message?: string;
+}
 
-function getFormFields(form: HTMLFormElement | null) {
-    const inputs = Array.from(form?.getElementsByTagName('input') || []);
-    const textareas = Array.from(form?.getElementsByTagName('textarea') || []);
+function getFormFields(form: HTMLFormElement) {
+    const inputs = Array.from(form.getElementsByTagName('input'));
+    const textareas = Array.from(form.getElementsByTagName('textarea'));
 
     return [...inputs, ...textareas];
 }
@@ -19,24 +21,33 @@ function createRequestBodyFromFormFields(fields: (HTMLInputElement | HTMLTextAre
     return body;
 }
 
+function displayMessage(wrapperElement: HTMLElement, message: string, isError: boolean) {
+    if (isError) wrapperElement.classList.add('text-red-700');
+    wrapperElement.innerText = message;
+    wrapperElement.classList.remove('hidden');
+}
+
 export default function handleNewsletterForm(
     url: string,
     formId: string,
-    errorId: string = 'error'
+    feedbackWrapperId: string = 'feedback'
 ) {
     const form = document.getElementById(formId) as HTMLFormElement | null;
 
     if (form) {
+        const client = createClient();
+        const formFields = getFormFields(form);
+        const feedbackWrapper = document.getElementById(feedbackWrapperId);
+
         form.addEventListener('submit', async (event: Event) => {
             event.preventDefault();
 
-            const formFields = getFormFields(form);
             const body = createRequestBodyFromFormFields(formFields);
-            const errorMessage = document.getElementById(errorId);
+            const { data, response } = await client<ResponseData>(url, body);
 
-            const response = await CLIENT(url, body);
-
-            if (errorMessage) errorMessage.innerText = response.message;
+            if (feedbackWrapper && data.message) {
+                displayMessage(feedbackWrapper, data.message, !response.ok);
+            }
         });
     }
 }
