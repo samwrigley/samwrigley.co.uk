42822fc3ef35e56632a792abe838c05ca37cc2b6
Sam Wrigley <sam@samwrigley.co.uk>
2020-10-29 20:07:02 +0000

    Moved constants to dedicated file

---
+++ resources/js/tests/consts.ts
@@ -0,0 +1,10 @@
+export const TEST_URL = 'https://example.com/test';
+
+export const TEST_INPUT_NAME = 'test-name';
+export const TEST_INPUT_VALUE = 'test-value';
+export const TEST_REQUEST_DATA = { [TEST_INPUT_NAME]: TEST_INPUT_VALUE };
+
+export const TEST_SUCCESS_RESPONSE_MESSAGE_TEXT = 'Success';
+export const TEST_SUCCESS_RESPONSE_DATA = { message: TEST_SUCCESS_RESPONSE_MESSAGE_TEXT };
+export const TEST_FAILURE_RESPONSE_MESSAGE_TEXT = 'Failure';
+export const TEST_FAILURE_RESPONSE_DATA = { message: TEST_FAILURE_RESPONSE_MESSAGE_TEXT };
--- resources/js/tests/serverHandlers.ts
+++ resources/js/tests/serverHandlers.ts
@@ -1,15 +1,11 @@
 import { rest } from 'msw';
-
-export const TEST_URL = 'https://example.com/test';
-
-export const TEST_INPUT_NAME = 'test-name';
-export const TEST_INPUT_VALUE = 'test-value';
-export const TEST_REQUEST_DATA = { [TEST_INPUT_NAME]: TEST_INPUT_VALUE };
-
-export const TEST_SUCCESS_RESPONSE_MESSAGE_TEXT = 'Success';
-export const TEST_SUCCESS_RESPONSE_DATA = { message: TEST_SUCCESS_RESPONSE_MESSAGE_TEXT };
-export const TEST_FAILURE_RESPONSE_MESSAGE_TEXT = 'Failure';
-export const TEST_FAILURE_RESPONSE_DATA = { message: TEST_SUCCESS_RESPONSE_MESSAGE_TEXT };
+import {
+    TEST_URL,
+    TEST_INPUT_NAME,
+    TEST_INPUT_VALUE,
+    TEST_FAILURE_RESPONSE_MESSAGE_TEXT,
+    TEST_SUCCESS_RESPONSE_DATA,
+} from './consts';
 
 interface Request {
     [key: string]: string;
