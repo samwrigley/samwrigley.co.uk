3f2243cbe7148a4c2987d7659144cadfbf6edf2e
Sam Wrigley <sam@samwrigley.co.uk>
2021-03-17 22:23:35 +0000

    Increase image failure threshold percentage

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,5 +2,5 @@ import '@testing-library/cypress/add-commands';
 import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
 
 addMatchImageSnapshotCommand({
-    failureThreshold: 0.03,
+    failureThreshold: 1,
 });
