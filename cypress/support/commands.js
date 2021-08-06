544a9ffa05f721b9c941d5aa08c91e4641f7b939
Sam Wrigley <sam@samwrigley.co.uk>
2021-08-06 14:37:27 +0100

    wip

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,5 +2,7 @@ import '@testing-library/cypress/add-commands';
 import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
 
 addMatchImageSnapshotCommand({
+    failureThreshold: 0.03,
+    failureThresholdType: 'percent',
     allowSizeMismatch: true,
 });
